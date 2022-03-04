0921610c88df6e84b63906a601ee5980c7b3cce0
gabrielres10 <93087966+gabrielres10@users.noreply.github.com>
2022-03-03 20:29:32 -0500

    Add files via upload

--- src/control/WindowB.java
+++ src/control/WindowB.java
@@ -13,105 +13,117 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class WindowB implements Initializable{
-	  @FXML
-	    private Button acceptBTN;
+public class WindowB implements Initializable {
 
-	    @FXML
-	    private TextField dateTF;
+	private String[] types = { "GASTO", "INGRESO" };
+	@FXML
+	private Button acceptBTN;
 
-	    @FXML
-	    private TextArea descTF;
+	@FXML
+	private TextField dateTF;
 
-	    @FXML
-	    private TextField typeTF;
+	@FXML
+	private TextArea descTF;
 
-	    @FXML
-	    private TextField valueTF;
+	@FXML
+	private TextField typeTF;
 
-	    @FXML
-	    void registerMoveBTN(ActionEvent event) {
-	    	
-	    	if(confirmDouble(dateTF.getText())) {
-	    		double value = Double.parseDouble(valueTF.getText());
-	    	}else {
-	    		Alert alert = new Alert(Alert.AlertType.WARNING);
-	    		alert.setTitle("Dato erróneo!");
-	            alert.setContentText("No puede agregar un monto con valores no admitidos");
-	            Optional<ButtonType> result = alert.showAndWait();
-	            
-	            clear();
-	    	}
-	    	Date date = confirmDate(dateTF.getText());
-	    	
-	    	//terminar de asignar campos de texto a variables del tipo correspondiente. Manejar la excepcion para el enum Type
-	    	
-	    	
-	    	
+	@FXML
+	private ChoiceBox<String> typeCB;
 
-	    }
+	@FXML
+	private TextField valueTF;
 
-		@Override
-		public void initialize(URL location, ResourceBundle resources) {
-			// TODO Auto-generated method stub
-			
+	@FXML
+	void registerMoveBTN(ActionEvent event) {
+
+		if (confirmDouble(dateTF.getText())) {
+			double value = Double.parseDouble(valueTF.getText());
+		} else {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Dato erróneo!");
+			alert.setContentText("No puede agregar un monto con valores no admitidos");
+			Optional<ButtonType> result = alert.showAndWait();
+
+			clear();
 		}
-		
-		/**
-		 * Convierte una cadena en tipo de dato Date
-		 * @param date, String, cadena a convertir
-		 * @return f, Date,  fecha correspondiente a la cadena
-		 */
-		public Date confirmDate(String date){
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			
-			Date f = new Date();
-			//crear excepcion para la siguiente parte
-			try {
-				f = sdf.parse(date);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				Alert alert = new Alert(Alert.AlertType.WARNING);
-	    		alert.setTitle("Dato erróneo!");
-	            alert.setContentText("El dato ingresado no tiene formato de fecha (yyyy-MM-dd)");
-	            Optional<ButtonType> result = alert.showAndWait();
-	            
-	            clear();
+		Date date = confirmDate(dateTF.getText());
+
+		// terminar de asignar campos de texto a variables del tipo correspondiente.
+		// Manejar la excepcion para el enum Type
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		completeCB();
+	}
+
+	public void completeCB() {
+		typeCB.getItems().addAll(types);
+	}
+
+	/**
+	 * Convierte una cadena en tipo de dato Date
+	 * 
+	 * @param date, String, cadena a convertir
+	 * @return f, Date, fecha correspondiente a la cadena
+	 */
+	public Date confirmDate(String date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		Date f = new Date();
+		// crear excepcion para la siguiente parte
+		try {
+			f = sdf.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Dato erróneo!");
+			alert.setContentText("El dato ingresado no tiene formato de fecha (yyyy-MM-dd)");
+			Optional<ButtonType> result = alert.showAndWait();
+
+			clear();
+		}
+		return f;
+
+	}
+
+	/**
+	 * Verifica que una cadena pueda pasarse a tipo double
+	 * 
+	 * @param value, String, cadena que se revisa
+	 * @return out, boolean, true si la cadena puede pasarse a double, false de lo
+	 *         contrario
+	 */
+	public boolean confirmDouble(String value) {
+
+		boolean out = true;
+		for (int i = 0; i < value.length(); i++) {
+			if (value.charAt(i) != '.' && value.charAt(i) != '1' && value.charAt(i) != '2' && value.charAt(i) != '3'
+					&& value.charAt(i) != '4' && value.charAt(i) != '5' && value.charAt(i) != '6'
+					&& value.charAt(i) != '7' && value.charAt(i) != '8' && value.charAt(i) != '9'
+					&& value.charAt(i) != '0') {
+				out = false;
+				break;
 			}
-			return f;
-			
 		}
-		
-		/**
-		 * Verifica que una cadena pueda pasarse a tipo double
-		 * @param value, String, cadena que se revisa
-		 * @return out, boolean, true si la cadena puede pasarse a double, false de lo contrario
-		 */
-		public boolean confirmDouble(String value) {
-			
-			boolean out = true;
-			for(int i = 0; i < value.length(); i++) {
-				if(value.charAt(i) != '.' && value.charAt(i) != '1' && value.charAt(i) != '2' && value.charAt(i) != '3' && value.charAt(i) != '4' && value.charAt(i) != '5' && value.charAt(i) != '6' && value.charAt(i) != '7' && value.charAt(i) != '8' && value.charAt(i) != '9' && value.charAt(i) != '0' ) {
-					out = false;
-					break;
-				}
-			}
-			return out;
-		}
-		
-		/**
-		 * Limpia todos los text field de la interfaz
-		 */
-		public void clear() {
-			dateTF.setText("");
-			descTF.setText("");
-			valueTF.setText("");
-			typeTF.setText("");
-		}
-	    
-	    
+		return out;
+	}
+
+	/**
+	 * Limpia todos los text field de la interfaz
+	 */
+	public void clear() {
+		dateTF.setText("");
+		descTF.setText("");
+		valueTF.setText("");
+		typeTF.setText("");
+	}
 
 }
